4ea2f8921d64af500bb394afad40610b008ea771
luis <lualreye@gmail.com>
2022-01-22 14:48:05 -0500

    Portfolio section component the portfolio secion was addded with the content, and the techonlogies stack added in about me too

--- src/components/index/AboutMe.vue
+++ src/components/index/AboutMe.vue
@@ -1,14 +1,57 @@
 <template>
   <div class="aboutMeSection">
-    Aui vamos a dar un parte de mi
+    <div class="aboutMeSection--container">
+      <h2>Me and my techies Skills</h2>
+      <p>My personal preferred techonolgies are:</p>
+      <ul>
+        <li v-for="(technology, index) in techonolgies" :key="index">
+          {{ technology.name }}
+        </li>
+      </ul>
+      <p>but i also i am proficient using techonologies as:</p>
+      <ul>
+        <li v-for="(el, index) in stack" :key="index">
+          {{ el.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  setup() {
+    const technologies = [
+      { name: "Nuxtjs" },
+      { name: "tailwind" },
+      { name: "firebase" },
+    ];
+    const stack = [
+      { name: "Vue2" },
+      { name: "Vue3" },
+      { name: "Javascript" },
+      { name: "Boostrap" },
+      { name: "Bulma" },
+      { name: "Git" },
+      { name: "Github" },
+      { name: "CSS" },
+    ];
+    const elementalStack = [
+      { name: "Nodejs" },
+      { name: "Expressjs" },
+      { name: "Postgressjs" },
+      { name: "Python" },
+    ];
 
-<style scoped>
-/* .aboutMeSection {
-  background: violet;
-} */
-</style>
 
 
+    return {
+      technologies,
+      stack,
+      elementalStack,
+    };
+  },
+};
+</script>
+
+<style scoped></style>

1876c23a8d5d33d7aeb8b2f88ba7704e1187e4c1
luis <lualreye@gmail.com>
2022-01-25 13:53:53 -0500

    feat: header component added to the project and styling according to the model

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <nav class="header--container">
+      <ul>
+        <li v-for="(item, index) in menu" :key="index">
+          <router-link :to="item.link">
+            {{ item.name }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    const menu = [
+      { name: '.is', link: '#' },
+      { name: '.Portfolio', link: '#' },
+      { name: '.abooutMe', link: '#' },
+      { name: '.contact', link: '#' },
+    ]
+
+    return {
+      menu
+    }
+  }
+}
+</script>

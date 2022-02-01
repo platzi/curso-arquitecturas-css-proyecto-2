aa0a5c803febebd6357a5d7f4e6b0ce17a609a61
luis <lualreye@gmail.com>
2022-02-01 12:43:17 -0500

    feat: threejs added to the project, and rendering in the route

--- src/components/My/Favourites.vue
+++ src/components/My/Favourites.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    aqui es donde va nuxt
+    <Renderer>
+      <Camera :position="{ z: 10}">
+        <Scene>
+          <PointLight :position="{ y: 50, z: 50}">
+            <Box>
+              <LambertMaterial />
+            </Box>
+          </PointLight>
+        </Scene>
+      </Camera>
+    </Renderer>
   </div>
 </template>
+
+<script>
+import { Box, Camera, PointLight,LambertMaterial, Renderer, Scene } from 'troisjs'
+export default {
+  components: {
+    Box,
+    Camera,
+    LambertMaterial,
+    Renderer,
+    Scene,
+    PointLight
+  }
+}
+</script>

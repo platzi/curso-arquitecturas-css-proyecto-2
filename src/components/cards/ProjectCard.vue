812abc408ee57ca875c65dbd001c624ffb5e9723
luis <lualreye@gmail.com>
2022-03-04 15:32:24 -0500

    feat: project card change according to the model

--- src/components/cards/ProjectCard.vue
+++ src/components/cards/ProjectCard.vue
@@ -1,11 +1,34 @@
 <template>
-  <button class="card" @click="getProject">
-    <div class="card--contentContainer">
-      <div class="wrapper">
-        <div class="img" :style="{ backgroundImage: 'url(' + bgImage + ')' }" />
+  <article class="projectCard-container">
+    <h3 class="project-title">
+      {{ title }}
+    </h3>
+    <p class="project-date">
+      {{ date }}
+    </p>
+    <figure class="project-image-container">
+      <img :src="image" :alt="title" class="project-image" />
+    </figure>
+    <div class="project-link-container">
+      <router-link :to="projectId" />
+      <div>
+        <svg
+          width="22"
+          height="8"
+          viewBox="0 0 22 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M21.8533 3.46459L18.6712 0.28267C18.476 0.0874119 18.1594 0.0874179 17.9641 0.282684C17.7689 0.47795 17.7689 0.794532 17.9642 0.989791L20.2926 3.31817L3.7286 3.31849C3.52388 2.75062 2.98027 2.34465 2.34185 2.34465C1.52795 2.34465 0.868164 3.00444 0.868164 3.81833C0.868164 4.63222 1.52795 5.29202 2.34185 5.29202C2.98016 5.29202 3.52369 4.88619 3.72849 4.31848L20.2926 4.31817L17.9643 6.64664C17.769 6.84191 17.769 7.15849 17.9643 7.35375C18.1595 7.54901 18.4761 7.549 18.6714 7.35374L21.8533 4.1717C22.0486 3.97643 22.0486 3.65985 21.8533 3.46459Z"
+            fill="#1E6F5C"
+          />
+        </svg>
       </div>
     </div>
-  </button>
+  </article>
 </template>
 
 <script>
@@ -13,8 +36,10 @@ import { useStore } from "vuex";
 import { computed } from "vue";
 export default {
   props: {
-    bgImage: String,
-    projectName: String,
+    image: String,
+    title: String,
+    date: String,
+    projectId: String,
   },
   setup(props) {
     const store = useStore();
@@ -28,7 +53,7 @@ export default {
         store.dispatch("modal/activeModal", false);
       } else {
         store.dispatch("modal/activeModal", true);
-        store.dispatch('projects/getSelectedProject', props.projectName)
+        store.dispatch("projects/getSelectedProject", props.projectName);
       }
     }
 
@@ -40,76 +65,52 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  padding: 1rem;
+.projectCard-container {
+  width: 386px;
+}
+
+.project-title {
+  letter-spacing: var(--leter-spacing);
+  color: var(--primary-color);
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 0.75rem;
+}
+
+.project-date {
+  letter-spacing: var(--letter-spacing);
+  color: var(--primary-color);
+  font-weight: 300;
+  font-family: var(--font-family);
+}
+
+.project-image-container {
+  width: 100%;
+  height: 210px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.project-image {
   width: 100%;
   height: 100%;
-  min-width: 254px;
-  min-height: 320px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.card--contentContainer {
-  background: #8f032a;
-  border-radius: 0.125rem;
-  padding: 0.15rem;
+  margin: 16px 0;
+  object-fit: cover;
+  object-position: top;
 }
 
-.card--contentContainer:hover {
-  box-shadow: 0px 32px 82px 4px rgba(143, 3, 42, 0.4);
-  -webkit-box-shadow: 0px 32px 82px 4px rgba(143, 3, 42, 0.4);
-  -moz-box-shadow: 0px 32px 82px 4px rgba(143, 3, 42, 0.4);
-}
-
-.wrapper {
-  background: white;
-  border-radius: 0.125rem;
+.project-link-container {
+  width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
 }
 
-.wrapper .img {
-  background-repeat: no-repeat;
-  background-position: top;
-  background-size: cover;
-  width: 480px;
-  height: 560px;
-}
-
-@media only screen and (min-width: 1441px) {
-  .wrapper .img {
-    width: 600px;
-  }
-}
-@media only screen and (min-width: 1025px) and (max-width: 1440px) {
-  .wrapper .img {
-    width: 420px;
-  }
-}
-
-@media only screen and (min-width: 767px) and (max-width: 1024px) {
-  .wrapper .img {
-    width: 300px;
-  }
-}
-
-@media only screen and (max-width: 540px) {
-  .wrapper .img {
-    width: 420px;
-  }
-}
-
-@media only screen and (min-width: 321px) and (max-width: 420px) {
-  .wrapper .img {
-    width: 320px;
-  }
-}
-
-@media only screen and (max-width: 320px) {
-  .wrapper .img {
-    width: 300px;
-  }
+.project-link {
+  color: var(--primary-color);
+  font-size: 0.75rem;
+  font-weight: 400;
+  margin-right: 8px;
 }
 </style>

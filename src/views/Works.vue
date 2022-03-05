388a901594443fb0b18454594ddc10c8dd3d678b
luis <lualreye@gmail.com>
2022-03-05 14:42:18 -0500

    feat: works view finished

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="works-container">
     <div class="works-container__title-container">
-      <h1 class="title">
-        TAKE A LOOK TO MY PORTFOLIO AND MY WORK
-      </h1>
+      <h1 class="title">TAKE A LOOK TO MY PORTFOLIO</h1>
+    </div>
+    <div class="works-container__projects-container">
+      <project-card
+        v-for="(project, index) in myProjects"
+        :key="index"
+        :title="project.name"
+        :image="project.image"
+        :date="project.date"
+        :projectId="project.id"
+        :position="index + 1"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import ProjectCard from "../components/cards/ProjectCard.vue"
 import { useStore } from "vuex";
 import { computed } from "vue";
 export default {
+  components: {
+    ProjectCard
+  },
   setup() {
     const store = useStore();
 
@@ -31,6 +44,7 @@ export default {
   width: 100%;
   max-width: 1440px;
   margin: 0 auto;
+  padding: 0 2.125rem;
 }
 .works-container__title-container {
   width: 100%;
@@ -44,5 +58,6 @@ export default {
   color: var(--primary-color);
   font-size: 4.5rem;
   font-weight: 500;
+  margin-bottom: -5%;
 }
 </style>

908dd5083b350708f562eca18311fd1a4844299f
luis <lualreye@gmail.com>
2022-03-05 14:26:18 -0500

    feat: Project view added and getting data from store

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -3,3 +3,31 @@
     projects details
   </div>
 </template>
+
+<script>
+import { useStore } from "vuex"
+import { useRoute } from "vue-router"
+import { ref, computed, onBeforeMount } from "vue"
+export default {
+  setup() {
+    const route = useRoute();
+    const store = useStore();
+    let pathId = ref("")
+
+    const routePath = onBeforeMount(() => pathId = route.path );
+
+    const getProjects = computed(() => {
+      return store.getters["projects/getProjects"]
+    })
+
+
+    return {
+      route,
+      pathId,
+      routePath,
+      store,
+      getProjects
+    };
+  },
+}
+</script>

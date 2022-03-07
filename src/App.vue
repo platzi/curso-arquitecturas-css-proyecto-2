aea17a9058dea9b5701458500aacd353d016fc31
luis <lualreye@gmail.com>
2022-03-07 09:03:58 -0500

    feat: footer created

--- src/App.vue
+++ src/App.vue
@@ -4,46 +4,46 @@
     <background />
     <the-header class="header-section" />
     <router-view />
+    <the-footer />
   </div>
 </template>
 
 <script>
 import TheHeader from "../src/components/TheHeader.vue";
-import ProjectModal from "../src/components/global/ProjectModal.vue"
-import Background from './components/global/Background.vue';
-import { useStore } from 'vuex'
-import { computed } from 'vue'
+import ProjectModal from "../src/components/global/ProjectModal.vue";
+import Background from "./components/global/Background.vue";
+import TheFooter from "./components/TheFooter.vue";
+import { useStore } from "vuex";
+import { computed } from "vue";
 export default {
   name: "App",
   components: {
     TheHeader,
     ProjectModal,
-    Background
+    Background,
+    TheFooter,
   },
   setup() {
-    const store = useStore()
+    const store = useStore();
 
     const isOpen = computed(() => {
-      return store.getters['config_drawer/getDrawer']
-    })
+      return store.getters["config_drawer/getDrawer"];
+    });
 
     return {
-      isOpen
-    }
-  }
+      isOpen,
+    };
+  },
 };
 </script>
 
 <style>
-@import './assets/css/variables.css';
+@import "./assets/css/variables.css";
 @import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,300&display=swap");
-
-
 
 .static {
   overflow: hidden;
 }
-
 
 .wrap {
   background: var(--bglight-color);
@@ -55,6 +55,4 @@ export default {
 .header-section {
   padding: 0 2.125rem;
 }
-
-
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,117 @@
+<template>
+  <footer class="footer-container">
+    <div class="footer-container__social-container">
+      <div
+        v-for="(item, index) in networks"
+        :key="index"
+        class="social-network"
+      >
+        <a :href="item.link" target="_blank">{{ item.name }}</a>
+      </div>
+    </div>
+    <div class="footer-container__copyright-container">
+      <p class="location">Guayaquil, Ecuador</p>
+      <p class="signature">2022, DEVELOPED BY LUIS REYES</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let networks = ref([
+      { name: "TWITTER", link: "https://twitter.com/lualreye" },
+      { name: "GITHUB", link: "https://github.com/lualreye" },
+      { name: "INSTAGRAM", link: "https://www.instagram.com/lualreye/" },
+    ]);
+
+    return {
+      networks: networks,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.footer-container {
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+  padding-right: 1rem;
+}
+
+.footer-container__social-container {
+  width: 320px;
+  padding: 0 2.125rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.social-network {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid var(--primary-color);
+  border-radius: 100%;
+  width: 4.5rem;
+  height: 4.5rem;
+  margin-right: 1rem;
+  padding: 1rem;
+}
+
+.social-network a {
+  text-align: center;
+  font-family: var(--font-family);
+  color: var(--primary-color);
+  font-size: 0.75rem;
+  font-weight: 300;
+  text-decoration: none;
+}
+
+.footer-container__copyright-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+}
+
+.location {
+  text-align: right;
+  font-family: var(--font-family);
+  color: var(--primary-color);
+  font-size: 0.75rem;
+  font-weight: 300;
+  margin: 0;
+  margin-bottom: 1rem;
+}
+
+.signature {
+  text-align: right;
+  font-family: var(--font-family);
+  color: var(--primary-color);
+  font-size: 0.75rem;
+  font-weight: 300;
+  margin: 0;
+}
+
+@media only screen and (max-width: 780px) {
+  .footer-container__copyright-container {
+    align-items: center;
+  }
+
+  .location {
+    text-align: center;
+  }
+
+  .signature {
+    text-align: center;
+  }
+}
+</style>

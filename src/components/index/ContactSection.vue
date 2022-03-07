6563a10072bb1e6badc4296732983f5c34185c8e
luis <lualreye@gmail.com>
2022-03-07 11:52:45 -0500

    fix: responsive design template fixed

--- src/components/index/ContactSection.vue
+++ src/components/index/ContactSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="contactSection">
-    <h2>Get in touch!</h2>
+    <h2 class="contactSection__title">HIT ME UP FOR WORKING TOGETHER</h2>
     <div class="contactSection__arrow-container">
       <button class="arrow" @click="activeContact">
         <the-icons v-if="isContactVisible" name="arrowDown" class="icon" />
@@ -9,39 +9,32 @@
     </div>
     <div v-if="!isContactVisible" class="contactSection--container">
       <form action="" class="form">
-        <div class="form--namesContainer">
-          <div class="inputContainer">
-            <label for="">Name</label>
-            <input
-              v-model="userName"
-              type="text"
-              name="name"
-              placeholder="John"
-            />
+        <div class="cotumer-data-container">
+          <div class="form--namesContainer">
+            <div class="inputContainer">
+              <label for="">NAME</label>
+              <input
+                v-model="userName"
+                type="text"
+                name="name"
+                placeholder="John"
+              />
+            </div>
           </div>
-          <div class="inputContainer">
-            <label for="">Lastname</label>
-            <input
-              v-model="userLastname"
-              type="text"
-              name="lastname"
-              placeholder="Doe"
-            />
-          </div>
-        </div>
-        <div class="form--emailContainer">
-          <div class="inputContainer">
-            <label for="">Email</label>
-            <input
-              v-model="userEmail"
-              type="email"
-              placeholder="johnDoe@cmail.com"
-            />
+          <div class="form--emailContainer">
+            <div class="inputContainer">
+              <label for="">EMAIL</label>
+              <input
+                v-model="userEmail"
+                type="email"
+                placeholder="johnDoe@cmail.com"
+              />
+            </div>
           </div>
         </div>
         <div class="messageContainer">
-          <div class="inputContainer">
-            <label for="">Message</label>
+          <div class="messageContainer__inputContainer">
+            <label for="">MESSAGE</label>
             <textarea
               v-model="userMessage"
               name=""
@@ -72,7 +65,7 @@ export default {
 
     const state = reactive({ isContactOpen: false });
 
-    const stateAsRefs = toRefs(state)
+    const stateAsRefs = toRefs(state);
 
     let isContactVisible = computed(() => state.isContactOpen);
 
@@ -122,7 +115,7 @@ export default {
       ...toRefs(mail),
       isContactVisible,
       activeContact,
-      stateAsRefs
+      stateAsRefs,
     };
   },
 };
@@ -137,10 +130,11 @@ export default {
   align-items: center;
   padding: 3rem 2.125rem;
 }
-.contactSection h2 {
-  font-family: "Nunito";
-  font-size: 6rem;
-  margin-bottom: 3rem;
+.contactSection__title {
+  font-family: var(--font-family);
+  color: var(--primary-color);
+  font-size: 4.5rem;
+  margin-bottom: 2rem;
 }
 
 .contactSection__arrow-container {
@@ -157,22 +151,18 @@ export default {
 }
 
 .arrow .icon {
-  color: var(--color-primary);
+  color: var(--primary-color);
 }
 
 .contactSection--container {
-  background: rgba(95, 116, 41, 1);
   width: 80%;
   max-width: 1024px;
-  border-radius: 2rem;
-  box-shadow: 1px 12px 53px -9px rgba(0, 0, 0, 0.75);
-  -webkit-box-shadow: 1px 12px 53px -9px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 1px 12px 53px -9px rgba(0, 0, 0, 0.75);
   padding: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
 }
+
 .form {
   width: 100%;
   padding: 0 0.25rem;
@@ -181,25 +171,50 @@ export default {
   align-items: center;
   flex-direction: column;
 }
+
 .form label {
-  font-family: "Nunito";
-  color: rgba(228, 199, 95, 1);
+  font-family: var(--font-family);
+  color: var(--primary-color);
+  font-size: 3rem;
+  font-weight: 500;
+  margin-right: 0.75rem;
   margin-bottom: 0.75rem;
 }
 
+.cotumer-data-container {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
 .form--namesContainer,
-.form--emailContainer,
+.form--emailContainer {
+  width: 50%;
+  display: flex;
+}
+
 .messageContainer {
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
 }
-.inputContainer {
+
+.messageContainer__inputContainer {
   display: flex;
   justify-content: center;
   align-items: flex-start;
-  flex-direction: column;
+  width: 100%;
+  margin-bottom: 1.25rem;
+  padding: 0 1rem;
+}
+
+.inputContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin-bottom: 1.25rem;
   width: 100%;
   padding: 0 1rem;
@@ -207,38 +222,41 @@ export default {
 .inputContainer input {
   width: 100%;
   background: transparent;
-  border: none;
-  border-bottom: olivedrab 2px solid;
-  font-family: "Nunito";
+  outline: none;
+  border: 2px solid var(--primary-color);
+  border-radius: 3rem;
+  padding: 0.75rem 0.5rem;
+  font-family: var(--font-family);
   font-size: 1.25rem;
-  color: #f5e6d7;
+  color: var(--primary-color);
 }
 .inputContainer input::placeholder {
   color: #a2a59f;
 }
+
 .inputContainer input:focus {
-  border: none;
   outline: none;
-  border-bottom: rgba(183, 157, 54, 1) 2px solid;
+  border: 2px solid var(--primary-color);
+  border-radius: 3rem;
 }
 .form textarea {
   margin: 0 0 2rem 0;
   background: transparent;
   width: 100%;
-  border: none;
-  border-bottom: olivedrab 2px solid;
-  font-family: "Nunito";
+  border: 2px solid var(--primary-color);
+  font-family: var(--font-family);
+  border-radius: 1.5rem;
+  padding: 0.75rem 0.5rem;
   font-size: 1.25rem;
-  color: #f5e6d7;
+  color: var(--primary-color);
 }
 .form textarea::placeholder {
   color: #a2a59f;
 }
 .form textarea:focus {
-  border: none;
-  border-bottom: rgba(228, 199, 95, 1) 2px solid;
   outline: none;
   font-size: 1.25rem;
+  color: var(--primary-color);
 }
 .form button {
   width: 100%;
@@ -258,21 +276,17 @@ export default {
   background: rgb(131, 114, 54);
 }
 @media only screen and (max-width: 1024px) {
-  .contactSection h2 {
+  .contactSection__title {
     font-size: 4rem;
   }
 }
-@media only screen and (max-width: 540px) {
-  .contactSection h2 {
+@media only screen and (max-width: 640px) {
+  .contactSection__title {
     font-size: 3rem;
+    text-align: center;
   }
   .form--namesContainer {
     flex-wrap: wrap;
-  }
-}
-@media only screen and (max-width: 375px) {
-  .contactSection h2 {
-    font-size: 2rem;
   }
 }
 </style>

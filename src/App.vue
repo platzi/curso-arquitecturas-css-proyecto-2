3485546aa11a8e1e08ce8c65e8dc4ce05333c6d3
luis <lualreye@gmail.com>
2022-03-10 23:24:42 -0500

    fix: loading component added until home is rendered

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
+  <div v-if="!isLoading" class="loading">
+    <loader />
+    <p class="loading-text">Loading . . .</p>
+  </div>
   <div class="wrap static">
     <the-header class="header-section" />
     <router-view v-slot="{ Component }">
-      <transition name="route" mode="out-in" >
+      <transition name="route" mode="out-in">
         <component :is="Component"></component>
       </transition>
     </router-view>
@@ -14,22 +18,39 @@
 import TheHeader from "../src/components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
 import { useStore } from "vuex";
-import { computed } from "vue";
+import { onMounted, computed, reactive, toRefs } from "vue";
+import Loader from "./components/global/Loader.vue"
 export default {
   name: "App",
   components: {
     TheHeader,
     TheFooter,
+    Loader
   },
   setup() {
     const store = useStore();
+
+    const renderState = reactive({
+      isLoading: false,
+    });
 
     const isOpen = computed(() => {
       return store.getters["config_drawer/getDrawer"];
     });
 
+    onMounted(() => {
+      loading();
+    });
+
+    function loading() {
+      setTimeout(() => {
+        renderState.isLoading = true;
+      }, 500);
+    }
+
     return {
       isOpen,
+      ...toRefs(renderState),
     };
   },
 };
@@ -43,6 +64,26 @@ export default {
   overflow: hidden;
 }
 
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 100;
+  background: var(--lightbg-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loading .loading-text {
+  font-family: var(--font-family);
+  font-size: 1.5rem;
+  color: var(--primary-color);
+  font-weight: 500;
+}
+
 .wrap {
   background: var(--bglight-color);
   position: relative;
@@ -53,7 +94,6 @@ export default {
 .header-section {
   padding: 0 2.125rem;
 }
-
 
 /* ROUTE TRANSITIONS */
 .route-enter-from {
@@ -73,5 +113,4 @@ export default {
 .route-leave-active {
   transition: all 0.3s ease-out;
 }
-
 </style>
